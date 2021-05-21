7423b89a431dd667cba9dd0c1a1e1e1fe10e0f76
Fabio Arango <farango0728@gmail.com>
2021-05-21 14:29:24 -0500

    configuracion de enpoint de usuarios

---
+++ bd/conexionBD.go
@@ -0,0 +1,37 @@
+package bd
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var MongoCN = ConectarBD()
+var clientOptions = options.Client().ApplyURI("mongodb+srv://Developem:kviCxtfQ28pKXrh@developem.xjkm8.mongodb.net/test?authSource=admin&replicaSet=atlas-53p1kx-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true")
+
+/*ConectarBD es la funcion que me permite conectar la BD*/
+func ConectarBD() *mongo.Client {
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	log.Println("Conexion Exitosa con la BD")
+	return client
+}
+
+/*ChequeoConnection es el Ping a la BD*/
+func ChequeoConnection() int {
+	err := MongoCN.Ping(context.TODO(), nil)
+	if err != nil {
+		return 0
+	}
+	return 1
+}
---
+++ handlers/handlers.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/farango0728/curso-go/middlew"
+	"github.com/farango0728/curso-go/routers"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+)
+
+/*Manejadores seteo mi puerto, el handle y pongo a escuchar al Servidor  */
+func Manejadores() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/registro", middlew.ChequeoBD(routers.Registro)).Methods("POST")
+
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		PORT = "8080"
+	}
+	handler := cors.AllowAll().Handler(router)
+	log.Fatal(http.ListenAndServe(";"+PORT, handler))
+
+}
